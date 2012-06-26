fff1b2bab02fa4d7294cf8ec944a65b043b7826c
Dieter Plaetinck <dieter@vimeo.com>
2012-06-26 17:15:01 +0200

    add default generic_var* tests

---
+++ tests/generic_var_string.sh
@@ -0,0 +1,23 @@
+# needed vars:
+# $config_sandbox
+# $key
+# $subject_process # 'pgrep -f'-compatible regex to find processes
+# $num_procs # expected number of processes
+# $error # error to look for (empty: assert no errors)
+# $config_backend
+
+test_pre () {
+        ${config_backend}_change_var $config_sandbox $key '"teststring"'
+}
+
+test_while () {
+        assert_num_procs "$subject_process" $num_procs
+}
+
+test_post () {
+        if [ -z "$error" ]; then
+                assert_no_errors;
+        else
+                assert_only_error "$error"
+        fi
+}
