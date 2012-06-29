1ac11d11ae8b1f02dc8b17d7cad96fedc9e1aa0c
Dieter Plaetinck <dieter@vimeo.com>
2012-06-29 15:37:42 +0200

    allow to run 1 specific testcase

--- sbb.sh
+++ sbb.sh
@@ -14,6 +14,7 @@ return=
 debug=0
 verbose=0
 pause=0
+case=
 
 # $1 exit code (default: 0)
 usage() {
@@ -31,13 +32,16 @@ EOF
     exit ${1:-0}
 }
 
-while getopts "hdpvc:" OPTION; do
+while getopts "hdpvc:C:" OPTION; do
      case $OPTION in
          h)
              usage
              ;;
          c)
              config="$OPTARG"
+             ;;
+         C)
+             case="$OPTARG"
              ;;
          d)
              debug=1
@@ -60,7 +64,11 @@ source "$config" || die_error "failed to source config $config"
 [ -n "$project" ] || die_error "\$project must be set to the name of your project"
 [ -d "$src" ] || die_error "\$src must be set to a directory containing your project, not '$src'"
 
-# run all tests that can run on their own
-for t in tests/!(generic_*); do
-        run_test $(basename $t .sh)
-done
+if [ -n "$case" ]; then
+        run_test $case
+else
+        # run all tests that can run on their own
+        for t in tests/!(generic_*).sh; do
+                run_test $(basename $t .sh)
+        done
+fi
