c66070de104f3a9e0938fc4605338c4b0d0a2620
Dieter Plaetinck <dieter@vimeo.com>
2012-07-05 15:14:48 +0200

    assert_pattern (): look for any pattern in given files

--- probes/output_patterns.sh
+++ probes/output_patterns.sh
@@ -27,3 +27,29 @@ assert_only_error () {
                 fail "no error matching '$error_match' in $*"
         fi
 }
+
+# $1 regex to match
+# $2 logged: 1/0 whether this pattern must be found or not
+# shift 2; $@ greppable things to look in
+assert_pattern () {
+        local match=$1
+        local logged=$2
+        [ -n "$match" ] || die_error "assert_pattern() \$1 must be a non-zero regex to match an error, not '$1'"
+        check_is_in $logged 0 1 || die_error "assert_pattern() needs the number 1 or 0 as \$2, not $2"
+        shift 2
+        at_least_one_readable_file 'assert_pattern() shift 2; $@' $@
+        num_match=$(grep -iR "$match" "$@" 2>/dev/null | wc -l)
+        if [ $num_match -gt 0 ]; then
+                if ((logged)); then
+                        win "$num_match string(s) matching '$match' in $*"
+                else
+                        fail "$num_match string(s) matching '$match' in $*"
+                fi
+        else
+                if ((logged)); then
+                        fail "no string matching '$match' in $*"
+                else
+                        win "no string matching '$match' in $*"
+                fi
+        fi
+}
