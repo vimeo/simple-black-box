7791ae46d4c4dfb346a502d1239676fccf655890
Dieter Plaetinck <dieter@vimeo.com>
2012-07-09 17:57:39 +0200

    update screenshot, add instructions+testcase for screenshots

---
+++ docs/testcase_for_screenshot.sh
@@ -0,0 +1,22 @@
+container=test_$RANDOM
+test_pre () {
+        debug "creating container $container: $(swift $swift_args post $container 2>&1)"
+        internal=1 assert_container_exists "$swift_args" $container
+        ${config_backend}_change_var $config_sandbox swift_container "\"$container\""
+}
+
+test_post () {
+        echo "Error: failed to check for existence of container $container" >> $stderr
+        assert_container_exists "$swift_args" $container
+        debug "deleting container $container: $(swift $swift_args delete $container 2>&1)"
+        internal=1 assert_container_exists "$swift_args" $container 0
+        assert_http_response_to "^PUT" 202 1
+        assert_num_http_requests "^PUT" 1 1
+        assert_http_response_to 'GET /auth/v1.0' 200
+        assert_num_http_requests 'GET /auth/v1.0' $num_procs_up $num_procs_up # every process will do an auth
+        assert_no_errors $stdout $stderr $log
+       # assert_pattern "container.*$container.*already existing" 1 $stdout $stderr $log
+        assert_pattern "created.*$container" 0 $stdout $stderr $log
+        assert_object_md5sum "$swift_args" myfiles file1.dat b6d81b360a5672d80c27430f39153e2c
+        debug_all_errors
+}
