54702c3a07a415698ca9899658af81bf147c303e
Dieter Plaetinck <dieter@vimeo.com>
2012-09-25 14:58:30 -0400

    adapt for swift uploader which checks statsd_host properly

--- tests/statsd_host_missing.sh
+++ tests/statsd_host_missing.sh
@@ -4,11 +4,11 @@ test_pre () {
 
 test_while () {
         assert_num_procs "$process_pattern_vega" 0
-        assert_num_procs "$process_pattern_uploader" $process_num_up_uploader
+        assert_num_procs "$process_pattern_uploader" 0
         assert_listening "$net_listen_addr" 0
 }
 
 test_post () {
-        test_post_vega_dies_during_startup "ERROR:.*missing.*statsd_host"
+        test_post_vega_and_uploader_die_during_startup "ERROR:.*missing.*statsd_host"
         debug_all_errors
 }
